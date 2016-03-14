a13f7aa8574b0f3c39c119334e2e886672cd45dd
ahassen <ahassen@apposit.com>
2016-03-14 15:39:47 +0300

    Added a timer to check for connection every 5 seconds

---
+++ background.js
@@ -0,0 +1,72 @@
+
+var state = 0; //variable to store the state of the Connection, 0 = no Connection; 1 = there is Connection
+var testUrl = 'http://www.google.com';
+
+var xhr = new XMLHttpRequest();
+
+window.setInterval(checkForConnection, 5000)
+
+function checkForConnection() {
+
+
+  xhr.open('GET', testUrl);
+
+xhr.onreadystatechange = function() {
+    if (xhr.readyState != 4) {
+    }else if (xhr.readyState == 4 && xhr.status == 200){
+      var newState = 1;
+      stateChanged(newState)
+    }
+    else if (xhr.readyState == 4 && xhr.status != 200){
+      var newState = 0;
+      stateChanged(newState)
+    }
+
+}
+
+  xhr.send(null);
+
+
+
+}
+
+function stateChanged(newState) {
+    var opt
+  if (newState == 1 && state == 0){
+      state = newState;//went from offline to online
+     opt = {
+        type: "basic",
+        title: "",
+        message: "Online!",
+        iconUrl: "icon64.png"
+      };
+  } else if (newState == 0 && state == 1){
+      state = newState;//went from online to offline
+     opt = {
+      type: "basic",
+      title: "",
+      message: "Offline!",
+      iconUrl: "icon64.png"
+    };
+
+  }
+    if(opt != null){
+        chrome.notifications.create(status, opt, creationCallback);
+    }
+
+}
+
+
+function creationCallback(notID) {
+    console.log("Succesfully created " + notID + " notification");
+}
+
+chrome.runtime.onMessage.addListener(function(request) {
+console.log("message");
+while (request.type === 'enabled') {
+  console.log("enabled");
+
+  checkForConnection();
+
+}
+    });
--- popup.js
+++ popup.js
@@ -1,41 +1,13 @@
-function creationCallback(notID) {
-console.log("Succesfully created " + notID + " notification");
-}
 
-function checkForConnection() {
-  var testUrl = 'http://www.google.com';
-
-  var xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function() {
-    if (xhr.readyState != 4 && xhr.status != 200) {
-      renderStatus("Checking for connection...");
-    }else if (xhr.readyState == 4 && xhr.status == 200){
-      renderStatus("Got connection!");
-      var opt = {
-  type: "basic",
-  title: "",
-  message: "Got Connection!",
-  iconUrl: "icon64.png"
-}
-      chrome.notifications.create('1', opt, creationCallback);
-
-
-    }
-    else if (xhr.readyState == 4 && xhr.status != 200){
-      renderStatus("No connection :(");
-
-    }
-}
-xhr.open('GET', testUrl);
-xhr.send(null);
-
-
-}
 
 function renderStatus(statusText) {
-  document.getElementById('status-label').textContent = statusText;
+  console.log(statusText)
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-    checkForConnection();
+  document.getElementById("check-now").addEventListener("click", function()
+  {
+    chrome.runtime.sendMessage({type:'enabled'});
+
+  }, false);
   });
